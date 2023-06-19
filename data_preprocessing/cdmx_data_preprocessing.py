db1b853f51f673c5c7993dc2c20556f09d4e2e42
Pieter Dietrich <pieter@dietrich-fam.de>
2023-06-19 17:41:29 -0600

    added the first preprocessing steps of my personal notebook to the cdmx_data_preprocessing..py file

--- data_preprocessing/cdmx_data_preprocessing.py
+++ data_preprocessing/cdmx_data_preprocessing.py
@@ -2,4 +2,21 @@ import numpy as np
 import pandas as pd
 
 
-def preprocess_data():
+
+url = "https://archivo.datos.cdmx.gob.mx/fiscalia-general-de-justicia/victimas-en-carpetas-de-investigacion-fgj/da_victimas_completa_marzo_2023.csv"
+data_cdmx = pd.read_csv(url)
+
+#sort out delicts that happened outside of cdmx
+boolean_mask = data_cdmx['municipio_hechos'].isnull()
+data = data_cdmx[boolean_mask].reset_index(drop=True)
+
+#sort out crimes that were not commited against persons but companies
+boolean_mask = data['TipoPersona'] == 'FISICA'
+data = data[boolean_mask].reset_index(drop=True)
+
+#columns to drop
+columns_to_drop = ['idCarpeta', 'Año_inicio', 'Mes_inicio', 'FechaInicio',
+                   'CalidadJuridica', 'Mes_hecho',  'HoraInicio',
+                  'fgj_colonia_registro', 'municipio_hechos']
+
+data = data.drop(columns_to_drop, axis=1)
