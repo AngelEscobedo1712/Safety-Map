c399f6865641eff8435fc18cae86f2c37eba6655
Theo Dehaze <theo.dehaze@gmail.com>
2023-06-20 14:58:19 -0400

    Added Data cleaning section to remove duplicates, filter Ano Hecho, Remove ages above 100 and replacing missing ages with mean, create unknown gender category, and clean missing or wrong latitudes and longitudes

--- data_preprocessing/cdmx_data_preprocessing.py
+++ data_preprocessing/cdmx_data_preprocessing.py
@@ -20,3 +20,32 @@ columns_to_drop = ['idCarpeta', 'Año_inicio', 'Mes_inicio', 'FechaInicio',
                   'fgj_colonia_registro', 'municipio_hechos']
 
 data = data.drop(columns_to_drop, axis=1)
+
+## DATA CLEANING
+
+# Remove duplicate rows
+data = data.drop_duplicates()
+
+
+# Remove Ano Hecho below 2019
+data = data[data["Año_hecho"] > 2018]
+
+
+# Remove age higher than 100 and replace all missing ages by mean age
+data['Edad'] = np.where(data['Edad'] > 100, np.nan, data['Edad'])
+
+data['Edad'] = np.where(data['Edad'].isna(), data["Edad"].mean(), data['Edad'])
+
+
+# Create Unknown gender category
+data['Sexo'] = np.where(data['Sexo'].isna(), "Unknown", data['Sexo'])
+
+# Very few gender unknowns, would drop
+
+
+# Assign missing latitude and longitude, set wrong longitudes to 0
+data['latitud'] = np.where(data['latitud'].isna(), 0, data['latitud'])
+
+data['longitud'] = np.where(data['longitud'].isna(), 0, data['longitud'])
+
+data['longitud'] = np.where(data['longitud'] == data["latitud"], 0, data['longitud'])
