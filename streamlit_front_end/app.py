dbf490c12155f81466f97fb9272568475e286df6
Pieter Dietrich <pieter@dietrich-fam.de>
2023-06-22 15:43:25 -0600

    first prototype works

--- streamlit_front_end/app.py
+++ streamlit_front_end/app.py
@@ -3,6 +3,7 @@ import datetime
 import requests
 import folium
 from google.cloud import bigquery
+from streamlit_folium import st_folium
 
 # Create a BigQuery client
 client = bigquery.Client()
@@ -10,18 +11,12 @@ client = bigquery.Client()
 # Define BigQuery project ID, dataset ID, and table ID
 project_id = "lucky-rookery-385417"
 dataset_id = "safety_map_new"
-table_id = "data_preprocessed"
+table_id = "map_data"
 column_name = "alcaldia_colonia"
 
 # Construct the BigQuery table reference
 table_ref = client.dataset(dataset_id).table(table_id)
 # Query to retrieve the column data
-
-
-
-
-
-
 
 # safety_map front
 
@@ -34,12 +29,9 @@ st.markdown("""
     with a safety map for this amazing city. The intention of our idea is to give you a clear overview of the neighborhoods where you
     eat tacos and trink mezcal tranquilito and where better not to go.
 
-
-
     Our data data includes all registered crimes in Colonias of CDMX from  **2019 to 2023**
 
     To be transparent from the beginning you can find the data for our project public available [here](https://datos.cdmx.gob.mx/dataset/victimas-en-carpetas-de-investigacion-fgj#:~:text=Descargar-,V%C3%ADctimas%20en%20Carpetas%20de%20Investigaci%C3%B3n%20(completa),-CSV)
-
 """)
 
 ###add map
@@ -47,24 +39,6 @@ st.markdown("""
 st.title("Historical crime data")
 
 map =folium.Map(location=[19.4326, -99.1332], zoom_start=11, tiles= 'Stamen Toner')
-
-
-###add drop downs
-
-#select year
-year = ['2019', '2020', '2021', '2022', '2023']
-
-selected_option = st.selectbox('Select a year:', year)
-
-
-#select month
-months = [
-    "January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"]
-
-selected_option = st.selectbox('Select a month:', months)
-
-
 
 ##select colonia (names queried from Big Query)
 query = f"SELECT DISTINCT {column_name} FROM `{project_id}.{dataset_id}.{table_id}`"
@@ -74,14 +48,58 @@ rows = query_job.result()
 # Extract the column values into a Python list
 colonias = [row[column_name] for row in rows]
 
-selected_option = st.selectbox('Select a Colonia:', colonias)
 
-##select crime
-crime = ['Burglary', 'Danger of well being','Domestic violence','Fraud','Homicide','Property damage',
-         'Robbery with violence','Robbery without violence','Sexual crime'
-]
-selected_option = st.selectbox('Select a Crime:', crime)
+###add drop downs
+dropdown_values = {
+    'year_column': [2019, 2020, 2021, 2022, 2023],
+    'month_column': list(range(1, 13)),
+    'alcaldia_colonia': colonias,
+    'Categoria': ['fraud', 'threats', 'threats', 'burglary', 'homicide',
+    'sexual crime', 'property damage', 'domestic violence', 'danger of well-being',
+    'robbery with violence', 'robbery without violence']
+}
 
 
-folium_map = map._repr_html_()
-st.components.v1.html(folium_map, width=700, height=500)
+
+# Collect selected values from the dropdown menus
+selected_values = {}
+for dropdown_label, dropdown_options in dropdown_values.items():
+    selected_values[dropdown_label] = st.selectbox(dropdown_label, dropdown_options)
+
+# Fetch the relevant data from BigQuery based on the selected values
+where_conditions = []
+for dropdown_label, selected_value in selected_values.items():
+    if isinstance(selected_value, str):
+        where_conditions.append(f"{dropdown_label} = '{selected_value}'")
+    else:
+        where_conditions.append(f"{dropdown_label} = {selected_value}")
+
+
+where_clause = " AND ".join(where_conditions)
+
+# Prepare the parameterized query
+query = f"""
+    SELECT latitud, longitud
+    FROM `{dataset_id}.{table_id}`
+    WHERE {where_clause}
+"""
+
+# Set query parameters with converted values
+query_parameters = [bigquery.ScalarQueryParameter(f"value{i}", "STRING", str(value)) for i, value in enumerate(selected_values.values())]
+
+# Run the query with parameterized values
+job_config = bigquery.QueryJobConfig()
+job_config.query_parameters = query_parameters
+query_job = client.query(query, job_config=job_config)
+dataframe = query_job.to_dataframe()
+
+# Create a Folium map
+map_center = [dataframe['latitud'].iloc[0], dataframe['longitud'].iloc[0]]
+m = folium.Map(location=map_center, zoom_start=10)
+
+# Add markers to the map
+for _, row in dataframe.iterrows():
+    folium.Marker([row['latitud'], row['longitud']]).add_to(map)
+
+# Display the map
+st_folium = st_folium(map, width = 700)
