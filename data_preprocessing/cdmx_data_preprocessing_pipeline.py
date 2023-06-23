64be42b80c01684bc82e9933dc48e3500d6d7fff
Pieter Dietrich <pieter@dietrich-fam.de>
2023-06-23 11:53:06 -0600

    preprocessing was finalized

--- data_preprocessing/cdmx_data_preprocessing_pipeline.py
+++ data_preprocessing/cdmx_data_preprocessing_pipeline.py
@@ -1,58 +1,57 @@
+import numpy as np
 import pandas as pd
-import json
+
 
 def download_data():
-    url = 'https://storage.googleapis.com/safetymap/data_new_cats.csv'
+    url = 'https://storage.googleapis.com/safety-map-model/clean_data.csv'
     clean_data = pd.read_csv(url)
     return clean_data
 
-data = download_data()
 
-data['alcaldia_colonia'] = data['alcaldia_hechos'].str.cat(data['colonia_datos'], sep=' || ')
+print("Data was succesfully downloaded")
+clean_data = download_data()
 
-#add year/month colum
-data['año_mes_hecho'] = pd.to_datetime(data['FechaHecho']).dt.to_period('M')
+clean_data['Date'] = pd.to_datetime(clean_data['Date'])
 
-#drop uncessary columns
-data.drop(columns=['Año_hecho', 'Mes_hecho', 'FechaHecho', 'HoraHecho', 'alcaldia_hechos', 'colonia_datos'], inplace=True)
+clean_data['year_month'] = clean_data['Date'].dt.strftime('%Y-%m')
+
+clean_data.drop(columns= 'Date')
 
 #group by Category and Colonia
-grouped_df = data.groupby(['año_mes_hecho', 'Categoria', 'alcaldia_colonia']).size().reset_index(name='count')
-
-#Change dtype
-grouped_df['año_mes_hecho'] = grouped_df['año_mes_hecho'].astype(str)
-
-grouped_df['año_mes_hecho'] = pd.to_datetime(grouped_df['año_mes_hecho'], format='%Y-%m')
+grouped_df = clean_data.groupby(['year_month', 'Category', 'Neighborhood']).size().reset_index(name='Count')
+grouped_df['year_month'] = pd.to_datetime(grouped_df['year_month'], format='%Y-%m')
 
 #create a df with the complete date range
-complete_dates= pd.date_range(start=grouped_df['año_mes_hecho'].min(), end = grouped_df['año_mes_hecho'].max(), freq = 'MS')
+complete_dates= pd.date_range(start=grouped_df['year_month'].min(), end = grouped_df['year_month'].max(), freq = 'MS')
+
+grouped_df['year_month'] = pd.to_datetime(grouped_df['year_month'], format='%Y-%m')
+
+#create a df with the complete date range
+complete_dates= pd.date_range(start=grouped_df['year_month'].min(), end = grouped_df['year_month'].max(), freq = 'MS')
 
 # Create all combinations of dates and colonia dates
-idx = pd.MultiIndex.from_product([grouped_df['alcaldia_colonia'].unique(), complete_dates, grouped_df['Categoria'].unique()], names=['alcaldia_colonia', 'año_mes_hecho', 'Categoria'])
+idx = pd.MultiIndex.from_product([grouped_df['Neighborhood'].unique(), complete_dates, grouped_df['Category'].unique()], names=['Neighborhood', 'year_month', 'Category'])
 complete_df = pd.DataFrame(index=idx).reset_index()
 
 #merge combinations with actual data
-colonias_date_complete_df = complete_df.merge(grouped_df, how='left', on=['alcaldia_colonia', 'año_mes_hecho', 'Categoria'])
-
+colonias_date_complete_df = complete_df.merge(grouped_df, how='left', on=['Neighborhood', 'year_month', 'Category'])
 
 #replace NaNs by 0
-colonias_date_complete_df['count'].fillna(0, inplace=True)
-
+colonias_date_complete_df['Count'].fillna(0, inplace=True)
 
 #Create A column for every crime category
-columns_colonias_date_complete_df = colonias_date_complete_df.pivot_table('count', ['alcaldia_colonia', 'año_mes_hecho'], 'Categoria')
+columns_colonias_date_complete_df = colonias_date_complete_df.pivot_table('Count', ['Neighborhood', 'year_month'], 'Category')
 columns_colonias_date_complete_df.reset_index(inplace=True)
 columns_colonias_date_complete_df.columns.names = ['Index']
 
 #Create unique IDs for each colonia
-columns_colonias_date_complete_df['colonia_id'] = pd.factorize(columns_colonias_date_complete_df['alcaldia_colonia'])[0] + 1
+columns_colonias_date_complete_df['Neighborhood_ID'] = pd.factorize(columns_colonias_date_complete_df['Neighborhood'])[0] + 1
 
 #create a dictionary with the ids for each colonia which we are going to use from now on
-codes, uniques = pd.factorize(columns_colonias_date_complete_df['alcaldia_colonia'].unique())
+codes, uniques = pd.factorize(columns_colonias_date_complete_df['Neighborhood'].unique())
 
 codes += 1
 colonia_id_dict = dict(zip(uniques, codes))
-
 
 #Convert floats to integer
 
@@ -62,4 +61,8 @@ columns_to_convert =['burglary', 'danger of well-being',
        'threats']
 columns_colonias_date_complete_df[columns_to_convert] = columns_colonias_date_complete_df[columns_to_convert].astype(int)
 
-columns_colonias_date_complete_df
+
+columns_colonias_date_complete_df.to_csv('/Users/pieterdietrich/code/AngelEscobedo1712/Safety-Map/Clean and Preprocessed Data/preprocessed_data.csv', index=False)
+
+
+print('preprocessed_data was downloaded')
