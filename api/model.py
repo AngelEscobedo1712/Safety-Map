2f7f454ceb1e651e11c2daa647566e85a9203106
angelescobedo <angelescobedo1712@gmail.com>
2023-06-23 11:52:15 -0600

    model.py updated

--- api/model.py
+++ api/model.py
@@ -181,37 +181,3 @@ print("We are Ready!, try with these predictions")
 
 # Save the model
 save_model(model)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Create feature and target arrays
-#X = np.array([[1], [2], [3], [4], [5], [6], [7]], dtype=float)
-#y = np.array([2, 3, 4, 5, 6, 7, 8], dtype=float)
-
-# Normalize data (this is important for neural networks)
-#X /= np.amax(X)
-#y /= np.amax(y)
-
-# Create a Sequential model
-#model = Sequential()
-
-# Add a Dense layer with 1 unit and input_shape of 1, and use the sigmoid activation function
-#model.add(Dense(1, input_shape=(1,), activation='sigmoid'))
-
-# Compile the model
-#model.compile(optimizer=Adam(), loss='mean_squared_error')
-
-# Fit the model
-#model.fit(X, y, epochs=200)
